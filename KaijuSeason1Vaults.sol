64bf106f4a7c58a0cd324f12bffa0d66bcc79abe
mattkaiju <matt@kaiju.fi>
2025-02-26 23:13:44 -0700

    Update KaijuSeason1Vaults.sol

--- KaijuSeason1Vaults.sol
+++ KaijuSeason1Vaults.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-import { MultiTokenBucketMiniVaultsUpgradeable } from "./UpgradableMultiTokenBucketMiniVaults.sol";
+import { MultiTokenVaultsUpgradable } from "./MultiTokenVaultsUpgradable.sol";
 
-contract KaijuSeason1Vaults is Initializable, UUPSUpgradeable, MultiTokenBucketMiniVaultsUpgradeable
+contract KaijuSeason1Vaults is Initializable, UUPSUpgradeable, UpgradableMultiTokenVaults
 {
     function initialize(address aaveAddress) public initializer 
     {
-        __Ownable_init(msg.sender);
+        __Ownable_init(_msgSender());
         __UUPSUpgradeable_init();
-        __MultiTokenBucketMiniVaultsUpgradeable_init(aaveAddress);
+        __UpgradableMultiTokenVaults_init(aaveAddress);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
