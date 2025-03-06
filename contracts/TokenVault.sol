771883799945f31382b767987adf729c4814b6c1
LAPTOP-8N82FJ5F\user <matt19sharp@gmail.com>
2025-03-06 18:05:36 +0800

    Moved file structure around

---
+++ contracts/TokenVault.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import { IPool } from "@aave/core-v3/contracts/interfaces/IPool.sol";
+import { DataTypes } from "@aave/core-v3/contracts/protocol/libraries/types/DataTypes.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { IKaijuKadoSoulBoundToken } from "./interfaces/IKaijuKadoSoulBoundToken.sol";
+
+contract TokenVault is Initializable, OwnableUpgradeable, ERC4626Upgradeable, ReentrancyGuardUpgradeable, UUPSUpgradeable
+{
+    using Math for uint256;
+
+    address private _aavePoolAddress;
+    address private _kaijuSoulboundTokenAddress;
+    uint256 private _giftThreshold;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        IERC20 asset, 
+        string memory name, 
+        string memory symbol, 
+        address aavePoolAddress,
+        address kaijuSoulboundTokenAddress,
+        uint256 giftThreshold
+    ) public initializer 
+    {
+        __ERC4626_init(asset);
+        __ERC20_init(name, symbol);
+        __ReentrancyGuard_init();
+        __Ownable_init(_msgSender());
+        __UUPSUpgradeable_init();
+
+        _aavePoolAddress = aavePoolAddress;
+        _kaijuSoulboundTokenAddress = kaijuSoulboundTokenAddress;
+        _giftThreshold = giftThreshold;
+    }
+
+    event AAVESupplied(address indexed user, address indexed token, uint256 amount, address tokenVaultAddress);
+    event AAVEWithdrawn(address indexed user, address indexed token, uint256 amount, address tokenVaultAddress);
+
+    function deposit(uint256 assets, address receiver) public nonReentrant override returns (uint256) 
+    {
+        uint256 maxAssets = maxDeposit(receiver);
+        if (assets > maxAssets) 
+        {
+            revert ERC4626ExceededMaxDeposit(receiver, assets, maxAssets);
+        }
+
+        uint256 shares = previewDeposit(assets);
+        _deposit(
+            _msgSender(), 
+            receiver, 
+            assets, 
+            shares
+        );
+
+        _supplyToPool(assets);
+
+        _giftSoulBoundToken(assets, receiver);   
+
+        return shares;
+    }
+
+    function withdraw(uint256 assets, address receiver, address owner) public nonReentrant override returns (uint256) 
+    {
+        uint256 maxAssets = maxWithdraw(owner);
+        if (assets > maxAssets) {
+            revert ERC4626ExceededMaxWithdraw(owner, assets, maxAssets);
+        }
+
+        _withdrawFromPool(assets);
+
+        uint256 shares = previewWithdraw(assets);
+        _withdraw(
+            _msgSender(), 
+            receiver, 
+            owner, 
+            assets, 
+            shares
+        );
+
+        return shares;
+    }
+
+    function withdrawTokenYield(address receiver, uint256 amount) public onlyOwner 
+    {
+        DataTypes.ReserveData memory reserveData = IPool(_aavePoolAddress).getReserveData(asset());
+
+        uint256 vaultATokenBalance = IERC20(reserveData.aTokenAddress).balanceOf(address(this));
+
+        require(vaultATokenBalance > totalSupply(), 'Nothing to withdraw');
+        require((vaultATokenBalance - totalSupply()) <= amount, 'Amount is larger than can be withdrawn');
+ 
+        IERC20(reserveData.aTokenAddress).transfer(receiver, amount);
+    }
+
+    function withdrawNativeYield(address receiver, uint256 amount) public onlyOwner 
+    {
+        payable(receiver).transfer(amount);
+    }
+
+    function _withdrawFromPool(uint256 amount) internal  
+    {
+        DataTypes.ReserveData memory reserveData = IPool(_aavePoolAddress).getReserveData(asset());
+
+        uint256 aTokenBalance = IERC20(reserveData.aTokenAddress).balanceOf(address(this));
+        require(aTokenBalance >= amount, "Insufficient aToken balance");
+
+        // Appprove IPool to burn lsts
+        IERC20(reserveData.aTokenAddress).approve(_aavePoolAddress, amount);
+
+        // Withdraw to user 
+        IPool(_aavePoolAddress).withdraw(
+            asset(), 
+            amount, 
+            address(this)
+        );
+        
+        // Fire event
+        emit AAVEWithdrawn(_msgSender(), asset(), amount, address(this));
+    }  
+
+    function totalAssets() public view override returns (uint256) 
+    {
+        return totalSupply(); 
+    }
+
+    function _supplyToPool(uint256 amount) internal
+    {
+        // Supply AAVE
+        IERC20(asset()).approve(_aavePoolAddress, amount);
+        IPool(_aavePoolAddress).supply(
+            asset(), 
+            amount, 
+            address(this),
+            0
+        );
+
+        // Fire event
+        emit AAVESupplied(_msgSender(), asset(), amount, address(this));
+    }
+
+    function _giftSoulBoundToken(uint256 assetsDeposited, address receiver) internal
+    {
+        // Get receiver balance
+        uint256 recieverBalance = IERC721Upgradeable(_kaijuSoulboundTokenAddress).balanceOf(receiver);
+
+        // Check receiver doesnt already have one and the amount deposited is greater than the threshold
+        if (recieverBalance <= 0 && assetsDeposited >= _giftThreshold) 
+        {
+            // Mint the receiver a soul bound token
+            IKaijuKadoSoulBoundToken(_kaijuSoulboundTokenAddress).mint(receiver);
+        }
+    }
+
+    function _convertToShares(uint256 assets, Math.Rounding) internal pure override returns (uint256) 
+    {
+        return assets; 
+    }
+
+    function _convertToAssets(uint256 shares, Math.Rounding) internal pure override returns (uint256) 
+    {
+        return shares; 
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override(UUPSUpgradeable) onlyOwner 
+    {}
+
+    receive() external payable 
+    {}
+}
---
+++ contracts/deprecated/MultiTokenVaultsUpgradable.sol
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import { TokenVault } from "./TokenVault.sol";
+
+abstract contract MultiTokenVaultsUpgradable is Initializable, AccessControlUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable, UUPSUpgradeable 
+{
+    /// @custom:storage-location erc7201:openzeppelin.storage.ERC4626
+    struct UpgradableMultiTokenVaultsUpgradeableStorage 
+    {
+        mapping(address => TokenVault) vaults;
+    }
+
+    // keccak256(abi.encode(uint256(keccak256("kaijufinance.storage.UpgradableMultiTokenVaultsUpgradeable")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant UpgradableMultiTokenVaultsUpgradeableStorageLocation = 0x2ca894d92e142fff7d00380311aa1e74c80eae52fdc58bc3f307e90a6ec91600;
+
+    event VaultCreated(address indexed token, address vault);
+    event Deposit(address indexed user, address indexed token, uint256 amount);
+    event Withdrawal(address indexed user, address indexed token, uint256 amount);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function _getUpgradableMultiTokenVaultsUpgradeableStorage() private pure returns (UpgradableMultiTokenVaultsUpgradeableStorage storage $) {
+        assembly {
+            $.slot := UpgradableMultiTokenVaultsUpgradeableStorageLocation
+        }
+    }
+
+    function __UpgradableMultiTokenVaults_init() internal onlyInitializing 
+    {
+        __AccessControl_init();
+        __ReentrancyGuard_init();
+        __Ownable_init(_msgSender());
+        __UUPSUpgradeable_init();
+        __UpgradableMultiTokenVaults_init_unchained();
+        
+        _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
+    }
+
+    function __UpgradableMultiTokenVaults_init_unchained() internal onlyInitializing 
+    { 
+    }
+
+    /*function createVault(address token) onlyOwner external 
+    {
+        UpgradableMultiTokenVaultsUpgradeableStorage storage $ = _getUpgradableMultiTokenVaultsUpgradeableStorage();
+
+        //require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Must have vault manager role to create vault");
+        require(address($.vaults[token]) == address(0), "Vault already exists");
+
+        TokenVault newVault = new TokenVault();
+        newVault.initialize(IERC20(token), $.aaveAddress);
+        $.vaults[token] = newVault;
+
+        emit VaultCreated(token, address(newVault));  
+    }*/
+
+    function createVault(address token, address payable vault) onlyOwner external 
+    {
+        UpgradableMultiTokenVaultsUpgradeableStorage storage $ = _getUpgradableMultiTokenVaultsUpgradeableStorage();
+
+        //require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Must have vault manager role to create vault");
+        require(address($.vaults[token]) == address(0), "Vault already exists");
+
+        $.vaults[token] = TokenVault(vault);
+
+        emit VaultCreated(token, address(vault));  
+    }
+
+    function deposit(address token, uint256 amount) external nonReentrant 
+    {
+        UpgradableMultiTokenVaultsUpgradeableStorage storage $ = _getUpgradableMultiTokenVaultsUpgradeableStorage();
+
+        TokenVault vault = $.vaults[token];
+        require(address(vault) != address(0), "Vault does not exist");
+        
+        IERC20(token).transferFrom(_msgSender(), address(this), amount);
+        IERC20(token).approve(address(vault), amount);
+        vault.deposit(amount, _msgSender());
+
+        emit Deposit(_msgSender(), token, amount);
+    }
+
+    function withdraw(address token, address receiver, uint256 shares) external nonReentrant
+    {
+        UpgradableMultiTokenVaultsUpgradeableStorage storage $ = _getUpgradableMultiTokenVaultsUpgradeableStorage();
+
+        require(address($.vaults[token]) != address(0), "Vault does not exist");
+
+        uint256 amount = $.vaults[token].convertToAssets(shares);
+        $.vaults[token].withdraw(amount, receiver, _msgSender());
+
+        emit Withdrawal(_msgSender(), token, amount);
+    }
+
+    function claimRewards(address token, address receiver, uint256 amount) onlyOwner external
+    {
+        UpgradableMultiTokenVaultsUpgradeableStorage storage $ = _getUpgradableMultiTokenVaultsUpgradeableStorage();
+
+        TokenVault vault = $.vaults[token];
+
+        vault.claimYield();
+        vault.withdrawTokenYield(receiver, amount);
+    }
+
+    function addRole(bytes32 role, address account) public onlyOwner {
+        require(role != DEFAULT_ADMIN_ROLE, "Cannot add DEFAULT_ADMIN_ROLE");
+        grantRole(role, account);
+    }
+
+    function removeRole(bytes32 role, address account) public onlyOwner {
+        require(role != DEFAULT_ADMIN_ROLE, "Cannot remove DEFAULT_ADMIN_ROLE");
+        revokeRole(role, account);
+    }
+ 
+    function _authorizeUpgrade(address newImplementation) internal virtual override onlyOwner {}
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(AccessControlUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
