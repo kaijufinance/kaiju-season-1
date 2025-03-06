0b8be179b4318424e625d9b142364a5a6434fa2e
mattkaiju <matt@kaiju.fi>
2025-03-06 12:17:40 +0800

    Update KaijuKadoSoulBoundToken.sol

--- KaijuKadoSoulBoundToken.sol
+++ KaijuKadoSoulBoundToken.sol
@@ -2,16 +2,18 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 import { IKaijuKadoSoulBoundToken } from "./interfaces/IKaijuKadoSoulBoundToken.sol";
 
-contract KaijuKadoSoulBoundToken is IKaijuKadoSoulBoundToken, Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract KaijuKadoSoulBoundToken is IKaijuKadoSoulBoundToken, Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
+    string private _baseTokenURI;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -20,16 +22,29 @@ contract KaijuKadoSoulBoundToken is IKaijuKadoSoulBoundToken, Initializable, ERC
 
     function initialize() public initializer {
         __ERC721_init("KaijuKadoSoulBoundToken", "KKSBT");
+        __ERC721URIStorage_init();
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
+
+        _baseTokenURI = "https://ipfs.io/ipfs";
     }
 
-    function mint(address to) public {
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function mint(address to) public 
+    {
         require(balanceOf(to) == 0, 'User already owns a soulbound token');
          
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
+        _setTokenURI(tokenId, 'QmbRvCwZKuJTVAXpHm4gsSBjCouzk9CPx4NSmn5X7qMuwy');
     }
 
     function _update(address to, uint256 tokenId, address auth)
@@ -42,12 +57,18 @@ contract KaijuKadoSoulBoundToken is IKaijuKadoSoulBoundToken, Initializable, ERC
         return super._update(to, tokenId, auth);
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        _requireOwned(tokenId);
-
-        string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string.concat(baseURI) : "";
+    function tokenURI(uint256 tokenId) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
+        return ERC721URIStorageUpgradeable.tokenURI(tokenId);
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function supportsInterface(bytes4 interfaceId) 
+        public 
+        view 
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable) 
+        returns (bool) 
+    {
+        return ERC721URIStorageUpgradeable.supportsInterface(interfaceId);
+    }
 }
